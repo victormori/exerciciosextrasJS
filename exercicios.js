173761a4491803adafb95f41711db38a37202f0b
victormori <victordaab@gmail.com>
2025-10-06 21:12:08 -0300

    exercicios em andamento

---
+++ exercicios.js
@@ -0,0 +1,56 @@
+// exercicios de JavaScript
+// Atividade1
+
+let Nome = "Victor";
+
+console.log("O" , Nome , "ama a Ana Beatriz")
+
+// Atividade2
+
+let nome = "Victor";
+let idade = 17;
+
+console.log("Meu nome é" , nome , "e eu tenho" , idade , "anos e amo a Ana Beatriz");
+
+// Atividade3
+
+let n = "Victor";
+let i = "17";
+let c = "Cruzeiro";
+
+console.log("Meu nome é" , n , ",eu tenho" , i , "anos e eu moro em" , c , ", e eu amo a Ana Beatriz");
+
+// Atividade4
+
+const valor = 3.14;
+const v1 = 5.5;
+const v2 = 6.7;
+
+console.log("O dobro de pi é:" , valor * 2);
+console.log("A soma de de 5.5 com 6.7 é igual:" , v1 + v2);
+
+// Operadores aritméticos
+// Atividade1
+
+const num1 = 450;
+const num2 = 50;
+
+console.log("A soma dos números 450 e 50 é:" , num1 + num2);
+console.log("A diferença entre os números 450 e 50 é:" , num1 - num2);
+console.log("O produto dos números 450 e 50 é:" , num1 * num2);
+console.log("A divisão entre os números 450 e 50 é:" , num1 / num2);
+console.log("O resto da divisão entre os números 450 e 50 é:" , num1 % num2);
+
+// Atividade2
+
+let nota1 = 10;
+let nota2 = 8.5;
+let nota3 = 7.5;
+let media = (nota1 + nota2 + nota3) /3
+
+console.log("A média das notas:" , media)
+
+// Atividade3
+
+
+
